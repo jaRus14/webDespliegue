5b30f6cefaa2402f42d8a87cfdd834dbb01a61fa
Aitor Sanchez <39703251+aitorSB@users.noreply.github.com>
2019-02-16 13:22:24 +0100

    final

--- comentarios.php
+++ comentarios.php
@@ -13,11 +13,12 @@
                 if(isset($_POST['submit']))  {
                     $nombre = $_POST['name'];
                     $comentario = $_POST['comentario'];
+                    $script = $_POST['script'];
                     $hora = date('H:i d/m/Y');
 
                     //preparamos stmt
-                    $stmt = $conexion->prepare("INSERT INTO coment(nombre, comentario, fecha_hora) VALUES (?, ?, ?)");
-                    $stmt->bind_param("sss", $nombre, $comentario, $hora);
+                    $stmt = $conexion->prepare("INSERT INTO coment(nombre, comentario, fecha_hora, script) VALUES (?, ?, ?, ?)");
+                    $stmt->bind_param("ssss", $nombre, $comentario, $hora, $script);
                     $stmt->execute();
                     $stmt->close();
                 }
@@ -27,7 +28,7 @@
                         while($row = mysqli_fetch_assoc($resultado)) {
                             echo '<li>';
                                 echo '<div class="commentText">';
-                                    echo "<p class='nombre'>".$row['nombre']."</p><p>".$row['comentario']."</p><span class='date sub-text'>".$row['fecha_hora']."</span>";
+                                    echo "<p class='nombre'>".$row['nombre']."</p><p>". $row['script']."</p><p>".$row['comentario']."</p><span class='date sub-text'>".$row['fecha_hora']."</span>";
                                 echo '</div>';
                             echo '</li>';
                         }
@@ -41,6 +42,7 @@
         <form class="form-inline" role="form" action="#" method="post">
             <div class="form-group">
                 <input type="text" class="form-control" name="name" placeHolder="Nombre (requerido)" required>
+                <input type="text" class="form-control" name="script" placeHolder="lenguaje" required>
                 <textarea class="form-control" maxlength="128" name="comentario" placeHolder="Escriba aquí su comentario máximo 128 caracteres"></textarea>
                 <input type="submit" class="btn btn-dark" name="submit" value="Añadir">
             </div>
